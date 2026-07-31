230450472998ad1c94a9770965c058c99ad7b95e
shakilaparvin932-sys <shakilaparvin932@gmail.com>
2026-07-31 17:15:52 +0600

    Create User.php

---
+++ app/Models/app/Models/app/Models/User.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable, HasRoles;
+
+    protected $fillable = [
+        'school_id',
+        'name',
+        'email',
+        'phone',
+        'password',
+        'status',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+
+    public function school()
+    {
+        return $this->belongsTo(School::class);
+    }
+}
