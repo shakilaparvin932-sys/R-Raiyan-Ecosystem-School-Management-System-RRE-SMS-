719b70636f8c9b7295de6646d4b0c6a23a6642f0
shakilaparvin932-sys <shakilaparvin932@gmail.com>
2026-07-31 17:19:15 +0600

    Create Section.php

---
+++ app/Models/app/Models/app/Models/app/Models/app/Models/app/Models/Section.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Section extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'school_id',
+        'class_id',
+        'section_name',
+    ];
+
+    public function school()
+    {
+        return $this->belongsTo(School::class);
+    }
+
+    public function schoolClass()
+    {
+        return $this->belongsTo(SchoolClass::class, 'class_id');
+    }
+}
